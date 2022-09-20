0afeeb839d8cbbb0d5c99c58ea83b5df6411511e
sandi <adithiasandijosua@gmail.com>
2022-09-20 15:54:34 +0700

    update json

--- pages/mente/index.jsx
+++ pages/mente/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Index = () => {
   return (
-    <div>test</div>
-  )
-}
+    <div>
+      <div className="row">
+        <div className="col-6 text-center">
+          <p>nama</p>
+          <p>kelas</p>
+          <p>jurusan</p>
+          <p>pendidikan</p>
+        </div>
+        <div className="col-6">
+          <p>telepon</p>
+          <p>telegram</p>
+          <p>discord</p>
+          <p>email</p>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
 
-export default Index
+export default Index;
