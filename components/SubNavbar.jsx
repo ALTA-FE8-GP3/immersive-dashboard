4d66fe934dc9870fe1414826270f7e2ef0f4feeb
Muhamad-Fatah <muhamad.fatah494@gmail.com>
2022-09-22 14:56:28 +0700

    udpate subnav

--- components/SubNavbar.jsx
+++ components/SubNavbar.jsx
@@ -5,8 +5,10 @@ import { Container, Row, Col, Badge } from "react-bootstrap"
 
 const SubNavbar = ({ title }) => {
     const [role, setRole] = useState()
+    const [user, setUser] = useState()
     useEffect(() => {
         setRole(getCookie("role"))
+        setUser(getCookie("user"))
     }, [])
 
     return (
@@ -18,7 +20,7 @@ const SubNavbar = ({ title }) => {
                         <p style={{ fontSize: '20px' }}>{title}</p>
                     </Col>
                     <Col xl={6}>
-                        <p className="text-end"><CgProfile style={{ fontSize: '40px', paddingRight: '10px' }} /> Hello, (Your name)</p>
+                        <p className="text-end"><CgProfile style={{ fontSize: '40px', paddingRight: '10px' }} /> Hello, {user}</p>
                         <Badge bg={role === "Admin" ? "success" : "info"} className="float-end">{role}</Badge>
                     </Col>
                 </Row>
